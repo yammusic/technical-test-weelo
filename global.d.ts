3760397b3237f0510a9b0367a32ea2e287f68c91
HackettYam <yeison.molina91@gmail.com>
2021-12-05 20:07:13 -0500

    Add global types

--- global.d.ts
+++ global.d.ts
@@ -1,7 +1,13 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /// <reference types="typescript" />
 
+import { ImageSourcePropType } from 'react-native'
 import { RootState as RootStateApp } from './src/store/index.d'
+
+declare module '*.png' {
+  const value: ImageSourcePropType
+  export default value
+}
 
 declare global {
   namespace CryptoCoinApp {
