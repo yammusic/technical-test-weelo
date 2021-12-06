1344ec57123cf76e30b41659e92b9d7e1a94ec23
HackettYam <yeison.molina91@gmail.com>
2021-12-05 20:14:43 -0500

    Fix align for currentPrice and changePercentage24h texts

--- src/components/commons/CryptoListItem/styles.ts
+++ src/components/commons/CryptoListItem/styles.ts
@@ -8,7 +8,7 @@ const styles = StyleSheet.create({
 
   itemRightContainer: {
     flexDirection: 'column',
-    alignItems: 'center',
+    alignItems: 'flex-end',
     justifyContent: 'center',
   },
 
